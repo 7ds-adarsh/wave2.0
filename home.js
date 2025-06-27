afb63c6fba22eeb95e00d5a5dddcbf5c33c499ef
Adarsh Pawaiya <adarshpawaiya345@gmail.com>
2025-06-27 15:46:55 +0530

    Add files via upload

---
+++ home.js
@@ -0,0 +1,43 @@
+
+// <!-- JS: Toggle mobile menu -->
+
+const menuToggle = document.getElementById('menu-toggle');
+const menuClose = document.getElementById('menu-close');
+const mobileMenu = document.getElementById('mobile-menu');
+
+menuToggle.addEventListener('click', () => {
+    mobileMenu.classList.remove('-translate-x-full');
+});
+
+menuClose.addEventListener('click', () => {
+    mobileMenu.classList.add('-translate-x-full');
+});
+
+// managing link redirections
+
+document.getElementById("logo").addEventListener("click", () => {
+    window.location.href = `${window.location.origin}/homepage/home.html`;
+});
+
+document.getElementById("nav-shop").addEventListener("click", () => {
+    window.location.href = `${window.location.origin}/plp/plp.html`;
+});
+
+document.getElementById("ntab-about").addEventListener("click", () => {
+    window.location.href = `${window.location.origin}/About/about.html`;
+});
+
+document.getElementById("login").addEventListener("click", () => {
+    window.location.href = `${window.location.origin}/login/login.html`;
+});
+
+// welcome on login tab
+
+const storedUSER = JSON.parse(localStorage.getItem('waveUser'));
+
+if (storedUSER && storedUSER.username) {
+  document.getElementById('welcome').textContent = `Welcome ${storedUSER.username}`;
+  document.getElementById('login').style.display = "none";
+} else {
+  document.getElementById('welcome').textContent = "Welcome";
+}
